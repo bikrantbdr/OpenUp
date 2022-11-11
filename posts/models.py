6b180aac5fd9be1ce8c9af5acfcd77375391b986
St-Luciferr <suntoss.pandey@gmail.com>
2022-11-11 15:35:55 +0545

    post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+
+class Posts(models.Model):
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    content = models.CharField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    support = models.models.IntegerField()
